d2d4d8fd061a9ace2fe182e1d9105f9ff86d1008
chrisclark <chris@untrod.com>
2015-08-29 18:26:49 -0700

    Couple helper functions from Steve Yegge

--- customizations/06-misc.el
+++ customizations/06-misc.el
@@ -27,3 +27,40 @@
 (autoload 'tidy-save-settings "tidy" "Save settings to `tidy-config-file'" t)
 (autoload 'tidy-build-menu  "tidy" "Install an options menu for HTML Tidy." t)
 
+;; Following tweaks stolen from https://sites.google.com/site/steveyegge2/my-dot-emacs-file
+
+;; Fix foolish calendar-mode scrolling.
+(add-hook 'calendar-load-hook
+ '(lambda ()
+ (setq mark-holidays-in-calendar t)
+ (define-key calendar-mode-map ">" 'scroll-calendar-left)
+ (define-key calendar-mode-map "<" 'scroll-calendar-right)
+ (define-key calendar-mode-map "\C-x>" 'scroll-calendar-left)
+ (define-key calendar-mode-map "\C-x<" 'scroll-calendar-right)))
+;;
+;; Never understood why Emacs doesn't have this function.
+;;
+(defun rename-file-and-buffer (new-name)
+ "Renames both current buffer and file it's visiting to NEW-NAME." (interactive "sNew name: ")
+ (let ((name (buffer-name))
+	(filename (buffer-file-name)))
+ (if (not filename)
+	(message "Buffer '%s' is not visiting a file!" name)
+ (if (get-buffer new-name)
+	 (message "A buffer named '%s' already exists!" new-name)
+	(progn 	 (rename-file name new-name 1) 	 (rename-buffer new-name) 	 (set-visited-file-name new-name) 	 (set-buffer-modified-p nil))))))
+
+;; Never understood why Emacs doesn't have this function, either.
+;;
+(defun move-buffer-file (dir)
+ "Moves both current buffer and file it's visiting to DIR." (interactive "DNew directory: ")
+ (let* ((name (buffer-name))
+	 (filename (buffer-file-name))
+	 (dir
+	 (if (string-match dir "\\(?:/\\|\\\\)$")
+	 (substring dir 0 -1) dir))
+	 (newname (concat dir "/" name)))
+
+ (if (not filename)
+	(message "Buffer '%s' is not visiting a file!" name)
+ (progn 	(copy-file filename newname 1) 	(delete-file filename) 	(set-visited-file-name newname) 	(set-buffer-modified-p nil) 	t))))
